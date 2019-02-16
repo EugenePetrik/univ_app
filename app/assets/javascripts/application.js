02aa3a0151420df60fe7d5a617831b4278bc89cf
Eugene Petrik <eugeniy.petrik@gmail.com>
2019-02-16 16:38:05 +0200

    Add flash messages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,7 @@
 //= require_tree .
 
 $( document ).on('turbolinks:load', function() {
-  $('.sidenav').sidenav();
+  $(".sidenav").sidenav();
   $(".dropdown-trigger").dropdown();
+  $("#fade-out-target").fadeOut(4000);
 })
