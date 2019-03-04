399d5bf7d42f14eef077aae4a3ed1c14d561d31d
EugeniyPetrik <eugeniy.petrik@gmail.com>
2019-03-04 17:19:18 +0200

    Add page object for Home page

---
+++ spec/support/pages/home_page.rb
@@ -0,0 +1,22 @@
+class HomePage < GeneralPage
+  set_url '/'
+
+  COURSE_CARD_SELECTOR = 'data-qa'.freeze
+
+  element :course_title, 'h3.center-align'
+  elements :course_cards, "div[#{COURSE_CARD_SELECTOR}]"
+
+  def course_cards_ids
+    course_cards.map { |course| course[COURSE_CARD_SELECTOR].to_i }
+  end
+
+  def course_enroll_with(course_id)
+    element = find(:xpath, "//div[@data-qa='#{course_id}']//a[text()='Enroll']")
+    element.click
+  end
+
+  def course_info_with(course_id)
+    element = find(:xpath, "//div[@data-qa='#{course_id}']//a[text()='Info']")
+    element.click
+  end
+end
