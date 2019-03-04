12c634aaff4354678fa04571e96654aa2d787db9
EugeniyPetrik <eugeniy.petrik@gmail.com>
2019-03-04 13:05:04 +0200

    Add settings for database_cleaner gem

---
+++ spec/support/config/database_cleaner.rb
@@ -0,0 +1,10 @@
+RSpec.configure do |config|
+  config.before :each do
+    if Capybara.current_driver == :rack_test
+      DatabaseCleaner.strategy = :transaction
+    else
+      DatabaseCleaner.strategy = :truncation
+    end
+    DatabaseCleaner.start
+  end
+end
